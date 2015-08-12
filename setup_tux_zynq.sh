bf34c4eed84f8d9c82d7d57949724fdcc136cb1c
Chris Hajas <hajas1@llnl.gov>
2015-08-12 20:57:43 +0000

    Added setup environment variables script

---
+++ setup_tux_zynq.sh
@@ -0,0 +1,3 @@
+export PATH=/opt/Xilinx/SDK/2014.1/bin:/opt/Xilinx/SDK/2014.1/gnu/microblaze/lin/bin:/opt/Xilinx/SDK/2014.1/gnu/arm/lin/bin:/cygdrive/c/Xilinx/SDK/2014.1/gnu/microblaze/linux_toolchain/nt64_be/bin:/opt/Xilinx/SDK/2014.1/gnu/microblaze/linux_toolchain/lin64_le/bin:$PATH
+export WORKSPACE_LOC=/home/hajas1/workspace
+export HOME=/home/hajas1
